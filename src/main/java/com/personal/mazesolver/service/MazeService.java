3a7c39ee00ce022ac74fc3df04e3b5ed9f3f08a8
jieguoyixia <zzhonglix@gmail.com>
2025-09-28 05:04:31 +0800

    generate maze naive

--- src/main/java/com/personal/mazesolver/service/MazeService.java
+++ src/main/java/com/personal/mazesolver/service/MazeService.java
@@ -8,12 +8,55 @@ import java.util.Random;
 // verb logic/actions perform on maze model
 @Service
 public class MazeService {
-
     private final Random random = new Random();
+    private static final int ROWS = 11;
+    private static final int COLS = 11;
 
+    // Generate a solvable maze
     public Maze generateMaze() {
+        int[][] grid = new int[ROWS * 2 + 1][COLS * 2 + 1];
+        boolean[][] visited = new boolean[ROWS][COLS];
 
+        // Fill walls
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                grid[i][j] = 1;
+            }
+        }
+
+        // Start and end positions
+        int[] start = {1, 1};
+        int[] end = {ROWS * 2 - 1, COLS * 2 - 1};
+
+        // Carve maze using recursive backtracking
+        carve(0, 0, ROWS, COLS, grid, visited);
+
+        return new Maze(ROWS, COLS, grid, start, end);
     }
 
-}
+    // Recursive backtracking
+    private void carve(int r, int c, int rows, int cols, int[][] grid, boolean[][] visited) {
+        int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
+        shuffle(directions);
+        visited[r][c] = true;
+        grid[r*2+1][c*2+1] = 0;
 
+        for (int[] d : directions) {
+            int nr = r + d[0], nc = c + d[1];
+            if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && !visited[nr][nc]) {
+                grid[r*2+1+d[0]][c*2+1+d[1]] = 0;
+                carve(nr, nc, rows, cols, grid, visited);
+            }
+        }
+    }
+
+    // Shuffle directions
+    private void shuffle(int[][] dirs) {
+        for (int i = dirs.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            int[] tmp = dirs[i];
+            dirs[i] = dirs[j];
+            dirs[j] = tmp;
+        }
+    }
+}
