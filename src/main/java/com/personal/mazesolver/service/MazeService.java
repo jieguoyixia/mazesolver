8cb4978e2add16e767dd15712910d0db371d187b
jieguoyixia <zzhonglix@gmail.com>
2025-09-28 19:12:27 +0800

    generate maze

--- src/main/java/com/personal/mazesolver/service/MazeService.java
+++ src/main/java/com/personal/mazesolver/service/MazeService.java
@@ -3,6 +3,7 @@ package com.personal.mazesolver.service;
 import com.personal.mazesolver.model.Maze;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.Random;
 
 // verb logic/actions perform on maze model
@@ -14,24 +15,25 @@ public class MazeService {
 
     // Generate a solvable maze
     public Maze generateMaze() {
-        int[][] grid = new int[ROWS * 2 + 1][COLS * 2 + 1];
+        int[][] grid = new int[ROWS * 2 + 1][COLS * 2 + 1]; //build cells n and walls
         boolean[][] visited = new boolean[ROWS][COLS];
-
-        // Fill walls
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                grid[i][j] = 1;
-            }
-        }
-
         // Start and end positions
         int[] start = {1, 1};
         int[] end = {ROWS * 2 - 1, COLS * 2 - 1};
+
+        initialMaze(grid);
 
         // Carve maze using recursive backtracking
         carve(0, 0, ROWS, COLS, grid, visited);
 
         return new Maze(ROWS, COLS, grid, start, end);
+    }
+
+    private void initialMaze(int[][] grid){
+        // Fill it with walls  all
+        for (int[] ints : grid) {
+            Arrays.fill(ints, 1);
+        }
     }
 
     // Recursive backtracking
